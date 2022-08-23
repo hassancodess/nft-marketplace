c7ae9b38c224829b9d730b9e6f6c09497fd23911
hassancodess <dev.hassancodes@gmail.com>
2022-08-23 22:47:36 +0500

    Add users data

---
+++ src/data/users.js
@@ -0,0 +1,53 @@
+import avatar1 from '../assets/avatars/avatar1.jpg'
+import avatar2 from '../assets/avatars/avatar2.jpg'
+import avatar3 from '../assets/avatars/avatar3.jpg'
+import avatar4 from '../assets/avatars/avatar4.jpg'
+import avatar5 from '../assets/avatars/avatar5.jpg'
+import avatar6 from '../assets/avatars/avatar6.jpg'
+import avatar7 from '../assets/avatars/avatar7.jpg'
+
+const users = [
+  {
+    name: 'Calvin Carlo',
+    handle: 'calvin_carlo',
+    gradient: 'bg-gradient-to-r from-green-300 via-blue-500 to-purple-600',
+    img: avatar1,
+  },
+  {
+    name: 'Floyd Glasgow',
+    handle: 'calvin_carlo',
+    gradient: 'bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500',
+    img: avatar2,
+  },
+  {
+    name: 'Tiffany Betancourt',
+    handle: 'calvin_carlo',
+    gradient: 'bg-gradient-to-r from-yellow-200 via-green-200 to-green-500',
+    img: avatar3,
+  },
+  {
+    name: 'Mary McMahon',
+    handle: 'calvin_carlo',
+    gradient: 'bg-gradient-to-r from-yellow-200 via-yellow-400 to-yellow-700',
+    img: avatar4,
+  },
+  {
+    name: 'Francis Younger',
+    handle: 'calvin_carlo',
+    gradient: 'bg-gradient-to-r from-indigo-300 to-purple-400',
+    img: avatar5,
+  },
+  {
+    name: 'Joshua Morris',
+    handle: 'calvin_carlo',
+    gradient: 'bg-gradient-to-r from-purple-200 via-purple-400 to-purple-800',
+    img: avatar6,
+  },
+  {
+    name: 'Donald Nielsen',
+    handle: 'calvin_carlo',
+    gradient: 'bg-gradient-to-r from-yellow-600 to-red-600',
+    img: avatar7,
+  },
+]
+export default users
