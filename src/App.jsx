b1622f9a530db2b32b591d2f8d5bc3b757a0d88b
hassancodess <dev.hassancodes@gmail.com>
2022-08-23 16:06:38 +0500

    add Best Sellers Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Steps from './components/Steps'
+import BestSellers from './components/BestSellers'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Steps />
+      <BestSellers />
     </>
   )
 }
