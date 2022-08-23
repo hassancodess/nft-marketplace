689010d34b778c39878f047e2f0020b70c3537bf
hassancodess <dev.hassancodes@gmail.com>
2022-08-23 22:25:31 +0500

    add data file for nfts

---
+++ src/data/nfts.js
@@ -0,0 +1,68 @@
+import nft1 from '../assets/nfts/nft1.jpg'
+import nft2 from '../assets/nfts/nft2.jpg'
+import nft3 from '../assets/nfts/nft3.jpg'
+import nft4 from '../assets/nfts/nft4.jpg'
+import nft5 from '../assets/nfts/nft5.jpg'
+import nft6 from '../assets/nfts/nft6.jpg'
+import nft7 from '../assets/nfts/nft7.jpg'
+import nft8 from '../assets/nfts/nft8.jpg'
+
+const nfts = [
+  {
+    img: nft1,
+    title: 'Deep Sea Phantasy',
+    price: '20.5',
+    likes: '22',
+    sale: true,
+  },
+  {
+    img: nft2,
+    title: 'CyberPrimal 042 LAN',
+    price: '40.5',
+    likes: '220',
+    sale: false,
+  },
+  {
+    img: nft3,
+    title: 'Crypto Egg Stamp',
+    price: '10.5',
+    likes: '122',
+    sale: false,
+  },
+  {
+    img: nft4,
+    title: 'Color Abstract Painting',
+    price: '4.3',
+    likes: '54',
+    sale: false,
+  },
+  {
+    img: nft5,
+    title: 'Liquid Forest Princess',
+    price: '2.9',
+    likes: '99',
+    sale: false,
+  },
+  {
+    img: nft6,
+    title: 'Spider Eyes Modern Art',
+    price: '7.7',
+    likes: '73',
+    sale: false,
+  },
+  {
+    img: nft7,
+    title: 'Synthwave Painting',
+    price: '3.4',
+    likes: '43',
+    sale: false,
+  },
+  {
+    img: nft8,
+    title: 'Contemporary Abstract',
+    price: '30.2',
+    likes: '91',
+    sale: false,
+  },
+]
+export default nfts
