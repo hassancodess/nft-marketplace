809065625b387e9638d3ead1c7f00bdb24f2cfad
hassancodess <dev.hassancodes@gmail.com>
2022-08-24 00:31:12 +0500

    add props to CTA Card Component

--- src/components/CTA/CTACardList.jsx
+++ src/components/CTA/CTACardList.jsx
@@ -4,8 +4,14 @@ function CTACardList() {
   return (
     <>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-        <CTACard />
-        <CTACard />
+        <CTACard
+          title='Join our community'
+          desc='We are a huge marketplace dedicated to connecting great artists of all Techwind.'
+        />
+        <CTACard
+          title='Learn more about Techwind'
+          desc='We are a huge marketplace dedicated to connecting great artists of all Techwind.'
+        />
       </div>
     </>
   )
