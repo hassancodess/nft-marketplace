01e617fd18f26d5196422f40b1899a8cf5d790a7
hassancodess <dev.hassancodes@gmail.com>
2022-08-25 00:54:14 +0500

    add Hero Animations

--- src/components/Header/Hero.jsx
+++ src/components/Header/Hero.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import hero from '../../assets/hero.png'
 
 function Hero() {
   const styles = {
@@ -7,28 +9,74 @@ function Hero() {
     btn: 'px-5 rounded-md font-medium border-indigo-600 py-2 bg-indigo-600 hover:bg-indigo-700 hover:border-indigo-700',
   }
 
+  const parentVariants = {
+    hidden: { x: -300, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: { when: 'beforeChildren', staggerChildren: 0.3 },
+    },
+  }
+  const imgVariants = {
+    hidden: { x: 500, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: { delay: 0.5 },
+    },
+  }
+  const childVariants = {
+    hidden: { x: -300, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+    },
+  }
+
   return (
     <>
       <section
         className={`py-36 p-4 md:py-52 relative overflow-hidden text-white ${styles.bgGradient}`}
       >
         {/* Backgound */}
-        <div className='bg-hero absolute inset-0 bg-no-repeat bg-bottom -z-10'></div>
+        <motion.div
+          variants={parentVariants}
+          initial='hidden'
+          animate='show'
+          className='absolute  inset-0 bg-no-repeat bg-bottom -z-10'
+        >
+          <motion.img
+            variants={imgVariants}
+            src={hero}
+            alt=''
+            className='object-cover w-full h-full'
+          />
+        </motion.div>
         {/* Content */}
         <div className='container max-w-6xl mx-auto'>
-          <div className='flex flex-col space-y-6 items-start'>
+          <motion.div
+            variants={parentVariants}
+            initial='hidden'
+            animate='show'
+            className='flex flex-col space-y-6 items-start'
+          >
             {/* Heading */}
-            <h1 className='text-5xl font-bold max-w-lg leading-normal'>
+            <motion.h1
+              variants={childVariants}
+              className='text-5xl font-bold max-w-lg leading-normal'
+            >
               Discover Exclusive Digital collectibles
-            </h1>
+            </motion.h1>
             {/* Paragraph */}
-            <p className='max-w-lg leading-6'>
+            <motion.p variants={childVariants} className='max-w-lg leading-6'>
               We are a huge marketplace dedicated to connecting great artists of
               all Techwind with their fans and unique token collectors!
-            </p>
+            </motion.p>
             {/* CTA */}
-            <button className={styles.btn}>Discover</button>
-          </div>
+            <motion.button variants={childVariants} className={styles.btn}>
+              Discover
+            </motion.button>
+          </motion.div>
         </div>
       </section>
     </>
