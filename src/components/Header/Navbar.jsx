e382d374e8032a85ebba4a1866f3be046425933a
hassancodess <dev.hassancodes@gmail.com>
2022-08-25 00:34:56 +0500

    add Navbar Animations

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { IoClose } from 'react-icons/io5'
 import navLinks from '../../data/navLinks'
+import { motion, AnimatePresence } from 'framer-motion'
 function Navbar() {
   const [nav, setNav] = useState(false)
   const toggleNav = () => {
@@ -15,18 +16,68 @@ function Navbar() {
     mobileNavLink:
       'hover:text-indigo-600 duration-200 ease-in-out border-b border-white  text-center',
   }
+
+  const containerVariants = {
+    hidden: { x: -10, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.2,
+      },
+    },
+  }
+  const linkVariants = {
+    hidden: { x: -10, opacity: 0 },
+    show: { x: 0, opacity: 1 },
+  }
+
+  const mobileContainer = {
+    hidden: { x: -50, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+    },
+  }
+  const mobileFlexContainer = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.2,
+      },
+    },
+  }
+  const mobileLinkVariants = {
+    hidden: { x: 0, opacity: 0 },
+    show: { x: 0, opacity: 1 },
+  }
   return (
     <>
       <nav className='p-4 bg-slate-900'>
         <div className='container mx-auto max-w-6xl relative'>
           {/* Flex Container */}
-          <div className='flex justify-between items-center text-white'>
+
+          <motion.div
+            variants={containerVariants}
+            initial='hidden'
+            animate='show'
+            className='flex justify-between items-center text-white'
+          >
             {/* Logo */}
-            <h1 className='text-2xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400'>
+            <motion.h1
+              variants={linkVariants}
+              className='text-2xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400'
+            >
               <span className=''>NFT</span> Marketplace.
-            </h1>
+            </motion.h1>
             {/* Menu Items */}
-            <div className='hidden lg:flex space-x-6 text-sm items-center'>
+            <motion.div
+              variants={linkVariants}
+              className='hidden lg:flex space-x-6 text-sm items-center'
+            >
               {navLinks.map((link, idx) => {
                 return (
                   <a key={idx} href='#' className={styles.navLink}>
@@ -36,30 +87,50 @@ function Navbar() {
               })}
 
               <button className={styles.btn}>Connect Wallet</button>
-            </div>
+            </motion.div>
             {/* Hamburger Menu */}
-            <div className='lg:hidden' onClick={toggleNav}>
+            <motion.div
+              variants={linkVariants}
+              className='lg:hidden'
+              onClick={toggleNav}
+            >
               {nav ? (
                 <IoClose size={28} className='text-white' />
               ) : (
                 <AiOutlineMenu size={25} />
               )}
-            </div>
+            </motion.div>
             {/* Mobile Menu */}
-            {nav && (
-              <div className='absolute top-14 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 z-50 w-full px-28 py-8 rounded-lg'>
-                <div className='flex flex-col items-center space-y-6 '>
-                  {navLinks.map((link, idx) => {
-                    return (
-                      <a key={idx} href='#' className={styles.mobileNavLink}>
-                        {link}
-                      </a>
-                    )
-                  })}
-                </div>
-              </div>
-            )}
-          </div>
+            <AnimatePresence>
+              {nav && (
+                <motion.div
+                  variants={mobileContainer}
+                  initial='hidden'
+                  animate='show'
+                  exit='hidden'
+                  className='absolute top-14 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 z-50 w-full px-28 py-8 rounded-lg'
+                >
+                  <motion.div
+                    variants={mobileFlexContainer}
+                    className='flex flex-col items-center space-y-6 '
+                  >
+                    {navLinks.map((link, idx) => {
+                      return (
+                        <motion.a
+                          variants={mobileLinkVariants}
+                          key={idx}
+                          href='#'
+                          className={styles.mobileNavLink}
+                        >
+                          {link}
+                        </motion.a>
+                      )
+                    })}
+                  </motion.div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </motion.div>
         </div>
       </nav>
     </>
