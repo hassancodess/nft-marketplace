b2a6894d250609faa33c5a9689c1c9361590b2e3
hassancodess <dev.hassancodes@gmail.com>
2022-08-25 01:16:03 +0500

    add animations to hot products component

--- src/components/HotProducts/HotProducts.jsx
+++ src/components/HotProducts/HotProducts.jsx
@@ -1,28 +1,78 @@
 import React from 'react'
 import NFTCardsList from './NFTCardsList'
 import { AiOutlineArrowRight } from 'react-icons/ai'
+import { motion } from 'framer-motion'
 
 function HotProducts() {
+  const parentVariants = {
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: { when: 'beforeChildren', staggerChildren: 0.5 },
+    },
+  }
+  const parentNFTVariants = {
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: { when: 'beforeChildren', staggerChildren: 0.15 },
+    },
+  }
+  const childVariants = {
+    hidden: {
+      x: 300,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      opacity: 1,
+      // transition: { delay: 0.4 },
+    },
+  }
   return (
     <>
       <section className='p-4 pb-24 text-white'>
         <div className='container max-w-6xl mx-auto overflow-hidden'>
           <div className='flex flex-col items-center space-y-8'>
             {/* Content */}
-            <div className='flex flex-col items-center space-y-4 '>
-              <h1 className='text-2xl md:text-3xl font-medium'>
+            <motion.div
+              variants={parentVariants}
+              initial='hidden'
+              animate='show'
+              className='flex flex-col items-center space-y-4 '
+            >
+              <motion.h1
+                variants={childVariants}
+                className='text-2xl md:text-3xl font-medium'
+              >
                 Explore Hot Products
-              </h1>
-              <p className='text-slate-400 max-w-lg text-center'>
+              </motion.h1>
+              <motion.p
+                variants={childVariants}
+                className='text-slate-400 max-w-lg text-center'
+              >
                 We are a huge marketplace dedicated to connecting great artists
                 of all Techwind with their fans and unique token collectors!
-              </p>
-            </div>
+              </motion.p>
+            </motion.div>
             {/* Collection of NFTs */}
-            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+            <motion.div
+              variants={parentNFTVariants}
+              initial='hidden'
+              animate='show'
+              className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8'
+            >
               {/* Card 1 */}
               <NFTCardsList />
-            </div>
+            </motion.div>
             <div className='md:flex items-center space-x-2 text-slate-400 font-semibold hidden  '>
               <p>Explore All Items</p>
               <AiOutlineArrowRight size={12} />
--- src/components/HotProducts/NFTCardsList.jsx
+++ src/components/HotProducts/NFTCardsList.jsx
@@ -1,19 +1,44 @@
 import React from 'react'
 import NFTCard from './NFTCard'
 import nfts from '../../data/nfts'
+import { motion } from 'framer-motion'
 function NFTCardsList() {
+  const parentVariants = {
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: { when: 'beforeChildren', staggerChildren: 0.1 },
+    },
+  }
+  const childVariants = {
+    hidden: {
+      x: 100,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      opacity: 1,
+      // transition: { delay: 0.1 },
+    },
+  }
   return (
     <>
       {nfts.map((nft) => {
         return (
-          <NFTCard
-            key={nft.title}
-            img={nft.img}
-            title={nft.title}
-            price={nft.price}
-            likes={nft.likes}
-            sale={nft.sale}
-          />
+          <motion.div variants={childVariants}>
+            <NFTCard
+              key={nft.title}
+              img={nft.img}
+              title={nft.title}
+              price={nft.price}
+              likes={nft.likes}
+              sale={nft.sale}
+            />
+          </motion.div>
         )
       })}
     </>
