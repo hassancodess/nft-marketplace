d3e6fad6187f38d1e697a7a4fe9643454eb60d26
hassancodess <dev.hassancodes@gmail.com>
2022-08-25 01:03:49 +0500

    Add Animations to Steps

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -1,28 +1,62 @@
 import React from 'react'
 import StepCard from './StepCard'
+import { motion } from 'framer-motion'
 function Steps() {
+  const parentVariants = {
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: { when: 'beforeChildren', staggerChildren: 0.5 },
+    },
+  }
+  const childVariants = {
+    hidden: {
+      x: -300,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      opacity: 1,
+      // transition: { },
+    },
+  }
   return (
     <>
       <section className='py-24 p-4'>
         <div className='container mx-auto max-w-6xl'>
           {/* Grid */}
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-white gap-6'>
-            <StepCard
-              title='Connect your wallet'
-              desc='Use Trust Wallet, Metamask or any wallet to connect to the app.'
-              icon='1'
-            />
-            <StepCard
-              title='Create your NFT Item'
-              desc='Upload your NFTs and set a title, description and price.'
-              icon='2'
-            />
-            <StepCard
-              title='List them for sale'
-              desc='Earn ETH and BIT for all your NFTs that you sell on our marketplace.'
-              icon='3'
-            />
-          </div>
+          <motion.div
+            variants={parentVariants}
+            initial='hidden'
+            animate='show'
+            className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-white gap-6'
+          >
+            <motion.div variants={childVariants}>
+              <StepCard
+                title='Connect your wallet'
+                desc='Use Trust Wallet, Metamask or any wallet to connect to the app.'
+                icon='1'
+              />
+            </motion.div>
+            <motion.div variants={childVariants}>
+              <StepCard
+                title='Create your NFT Item'
+                desc='Upload your NFTs and set a title, description and price.'
+                icon='2'
+              />
+            </motion.div>
+            <motion.div variants={childVariants}>
+              <StepCard
+                title='List them for sale'
+                desc='Earn ETH and BIT for all your NFTs that you sell on our marketplace.'
+                icon='3'
+              />
+            </motion.div>
+          </motion.div>
         </div>
       </section>
     </>
